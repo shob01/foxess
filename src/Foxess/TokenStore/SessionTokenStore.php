49f8601d002146f87489ef24645b8235e0881f36
shob01 <axel.schober@gmx.de>
2023-08-31 12:40:50 +0200

    Check for existing session before session_start()

--- src/Foxess/TokenStore/SessionTokenStore.php
+++ src/Foxess/TokenStore/SessionTokenStore.php
@@ -8,16 +8,16 @@ class SessionTokenStore implements ITokenStore
 
     public function __construct()
     {
-       session_start(); 
+        if (session_status() !== PHP_SESSION_ACTIVE)
+            session_start();
     }
-    
-    public function get() : string
+
+    public function get(): string
     {
         return isset($_SESSION[self::TOKEN_NAME]) ? $_SESSION[self::TOKEN_NAME] : '';
     }
-    public function store(string $token) : void
+    public function store(string $token): void
     {
-        $_SESSION[self::TOKEN_NAME] = $token; 
+        $_SESSION[self::TOKEN_NAME] = $token;
     }
 }
-?>
